8c268e4a19e0ee87339d76dcbe84104f431496fc
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-04-23 16:14:18 -0400

    Js work

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ let myLibrary = [
     "https://i.harperapps.com/hcanz/covers/9780261102668/y648.jpg"
   ),
   new Book(
-    "The Hobbit",
-    "J.R.R. Tolkien",
-    295,
+    "The Three-Body Problem",
+    "Liu Cixin",
+    399,
     true,
-    "https://i.harperapps.com/hcanz/covers/9780261102668/y648.jpg"
+    "https://images-na.ssl-images-amazon.com/images/I/919XM42JQlL.jpg"
   ),
+  new Book("The Three-Body Problem", "Liu Cixin", 399, true),
 ];
 
 function Book(title, author, pages, read, img) {
@@ -23,16 +24,9 @@ function Book(title, author, pages, read, img) {
   this.img = img;
 }
 
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${
-    this.read ? "read." : "not read yet."
-  }`;
-};
-
-function addBookToLibrary() {}
-
 function showBooks() {
   const library = document.querySelector(".library");
+
   for (const i of myLibrary) {
     let book = document.createElement("div");
     let title = document.createElement("div");
@@ -40,7 +34,6 @@ function showBooks() {
     let pages = document.createElement("div");
     let read = document.createElement("div");
 
-    book.className = "book";
     title.className = "title";
     author.className = "author";
     pages.className = "pages";
@@ -56,12 +49,31 @@ function showBooks() {
     pages.innerHTML = i.pages;
     read.innerHTML = "Read";
 
-    library.appendChild(book);
+    /* Handle img */
+    if (i.img === undefined) {
+      book.className = "book--no-img";
+    } else {
+      let img = document.createElement("img");
+      book.appendChild(img);
+      img.src = i.img;
+      book.className = "book";
+    }
 
-    console.log(book);
+    library.appendChild(book);
   }
 }
 
+function addBookToLibrary() {
+  /*let title = prompt("Title");
+  let author = prompt("Author");
+  let pages = prompt("Pages");
+  let read = prompt("Have you read it?");*/
+
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
+
+//addBookToLibrary();
 showBooks();
 
 console.log(myLibrary);
