a870a99d66419318c1295cc8906266fb56cfd522
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-04-24 15:17:53 -0400

    remove book functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ document.querySelector(".btn-add").addEventListener("click", showAddForm);
 
 let myLibrary = [
   new Book(
+    0,
     "The Hobbit",
     "J.R.R. Tolkien",
     295,
@@ -10,6 +11,7 @@ let myLibrary = [
     "https://i.harperapps.com/hcanz/covers/9780261102668/y648.jpg"
   ),
   new Book(
+    1,
     "The Three-Body Problem",
     "Liu Cixin",
     399,
@@ -17,6 +19,7 @@ let myLibrary = [
     "https://images-na.ssl-images-amazon.com/images/I/919XM42JQlL.jpg"
   ),
   new Book(
+    2,
     "The Three-Body Problem",
     "Liu Cixin",
     399,
@@ -25,7 +28,8 @@ let myLibrary = [
   ),
 ];
 
-function Book(title, author, pages, read, img) {
+function Book(id, title, author, pages, read, img) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -34,31 +38,42 @@ function Book(title, author, pages, read, img) {
 }
 
 function showBooks() {
+  /* --- Get library div and clear it --- */
   const libraryDiv = document.querySelector(".library");
   libraryDiv.innerHTML = "";
+
   for (const i of myLibrary) {
+    let id = document.createElement("div");
     let book = document.createElement("div");
     let title = document.createElement("div");
     let author = document.createElement("div");
     let pages = document.createElement("div");
     let read = document.createElement("div");
+    let btnDelete = document.createElement("div");
 
+    id.className = "book-id";
     title.className = "title";
     author.className = "author";
     pages.className = "pages";
     read.className = "read";
+    btnDelete.className = "btn-delete";
 
-    book.appendChild(title);
-    book.appendChild(author);
-    book.appendChild(pages);
-    book.appendChild(read);
-
+    id.dataset.bookId = i.id;
     title.innerHTML = i.title;
     author.innerHTML = i.author;
     pages.innerHTML = i.pages;
     read.innerHTML = "Read";
+    btnDelete.innerHTML = "Remove";
+    btnDelete.addEventListener("click", deleteBook);
 
-    /* Handle img */
+    book.appendChild(id);
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(read);
+    book.appendChild(btnDelete);
+
+    /* --- Handle img --- */
     if (i.img === undefined) {
       book.className = "book--no-img";
     } else {
@@ -73,6 +88,15 @@ function showBooks() {
 }
 
 function addBookToLibrary() {
+  /* --- Set book id --- */
+  let newId = 0;
+  for (const i of myLibrary) {
+    if (i.id > newId) {
+      newId = i.id;
+    }
+  }
+  newId++;
+
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
   let pages = document.querySelector("#pages").value;
@@ -82,9 +106,9 @@ function addBookToLibrary() {
   console.log(img);
   let book;
   if (img === undefined) {
-    book = new Book(title, author, pages, read);
+    book = new Book(newId, title, author, pages, read);
   } else {
-    book = new Book(title, author, pages, read, img);
+    book = new Book(newId, title, author, pages, read, img);
   }
 
   myLibrary.push(book);
@@ -92,10 +116,15 @@ function addBookToLibrary() {
   showBooks();
 }
 
+function deleteBook(e) {
+  let idToDelete = e.target.parentNode.querySelector(".book-id").dataset.bookId;
+  myLibrary = myLibrary.filter((book) => book.id != idToDelete);
+  console.log(myLibrary);
+  showBooks();
+}
+
 function showAddForm() {
   let modal = (document.querySelector(".modal").style.display = "flex");
 }
 
-//addBookToLibrary();
 showBooks();
-console.log(myLibrary);
